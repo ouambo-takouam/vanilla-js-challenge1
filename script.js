f6d51b1500842f8ebeef17b842e285137bff10fa
patrick ouambo <patrickouambo@gmail.com>
2023-07-26 16:40:17 +0200

    frst commit for this challenge

---
+++ script.js
@@ -0,0 +1,45 @@
+const countEl1 = document.getElementById('count1-el');
+const countEl2 = document.getElementById('count2-el');
+
+var count1 = 0;
+var count2 = 0;
+
+function updateCount1() {
+	countEl1.textContent = count1;
+}
+
+function incrementByOne() {
+	count1++;
+	updateCount1();
+}
+
+function incrementByTwo() {
+	count1 += 2;
+	updateCount1();
+}
+
+function incrementByThree() {
+	count1 += 3;
+	updateCount1();
+}
+
+// GUEST
+
+function updateCount2() {
+	countEl2.textContent = count2;
+}
+
+function addOne() {
+	count2++;
+	updateCount2();
+}
+
+function addTwo() {
+	count2 += 2;
+	updateCount2();
+}
+
+function addThree() {
+	count2 += 3;
+	updateCount2();
+}
